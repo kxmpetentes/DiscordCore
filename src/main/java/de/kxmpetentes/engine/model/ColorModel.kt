79dab3a49c8832c56ca2015d80a817a5d0aaa228
kxmpetentes <mikkakohnz@gmail.com>
2021-02-24 10:54:17 +0100

    ColorModel.kt fixed Nullpointer

--- src/main/java/de/kxmpetentes/engine/model/ColorModel.kt
+++ src/main/java/de/kxmpetentes/engine/model/ColorModel.kt
@@ -5,8 +5,6 @@ import kotlin.random.Random
 
 data class ColorModel(private val colors: Array<Color>) {
 
-
-
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -23,7 +21,7 @@ data class ColorModel(private val colors: Array<Color>) {
     }
 
     fun getColor(): Color {
-        return colors[Random.nextInt()]
+        return colors[Random.nextInt(colors.size)]
     }
 
 }
