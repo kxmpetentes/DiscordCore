5d6316f5b6ff8e2c0bc440d4b81d8e21c38053da
spotifynutzeer <paul.j.weber@outlook.de>
2021-03-03 17:45:17 +0100

    Added Hastebin.kt

--- src/main/java/de/kxmpetentes/engine/utils/Hastebin.kt
+++ src/main/java/de/kxmpetentes/engine/utils/Hastebin.kt
@@ -1,8 +1,53 @@
 package de.kxmpetentes.engine.utils
 
+import java.io.*
+import java.net.URL
+import java.nio.charset.StandardCharsets
+import javax.net.ssl.HttpsURLConnection
 
-public class Hastebin {
-  constructor(String message) {
-    TODO("Create code for HasteBin")
+
+class Hastebin {
+
+
+  fun post(message: String, url: String, raw: Boolean): String {
+    val postData: ByteArray = message.toByteArray(StandardCharsets.UTF_8)
+    val postDataLength: Int = postData.size
+
+    val url: URL = URL(url)
+    val connection: HttpsURLConnection = url.openConnection() as HttpsURLConnection
+    connection.doOutput = true
+    connection.instanceFollowRedirects = false
+    connection.requestMethod = "POST"
+    connection.setRequestProperty("User-Agent", "Hastebin Java API")
+    connection.setRequestProperty("Content-Length", postDataLength.toString())
+    connection.useCaches = false
+
+    var response: String = ""
+    val wr: DataOutputStream
+
+    try {
+      wr = DataOutputStream(connection.outputStream)
+      wr.write(postData)
+      val reader: BufferedReader = BufferedReader(InputStreamReader(connection.inputStream))
+      response = reader.readLine()
+    } catch (exception: IOException) {
+      exception.printStackTrace()
+    }
+
+    if (response.contains("\"key\"")) {
+      response = response.substring(response.indexOf(":") + 2, response.length - 2)
+
+      val postUrl: String
+      if (raw) {
+        postUrl = "${url.toString()}/raw"
+      } else {
+        postUrl = url.toString()
+      }
+
+      response = postUrl + response
+    }
+
+    return response
   }
+
 }
