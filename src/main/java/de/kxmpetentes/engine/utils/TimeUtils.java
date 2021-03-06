5787590bf23a3e554d921f9cc7eb40f8df155324
kxmpetentes <mikkakohnz@gmail.com>
2021-03-06 17:03:06 +0100

    getRelativeTime Method simplified
    Signed-off-by: kxmpetentes <mikkakohnz@gmail.com>

--- src/main/java/de/kxmpetentes/engine/utils/TimeUtils.java
+++ src/main/java/de/kxmpetentes/engine/utils/TimeUtils.java
@@ -40,60 +40,37 @@ public class TimeUtils {
 
     /**
      * @param time timestamp in seconds
-     * @return string of x min ago
-     */
-    public static String getRelativeTime(long time) {
-        return getRelativeTime(time, true);
-    }
-
-    /**
-     * @param time      timestamp in seconds
-     * @param shortText short or long text
-     * @return string of x min ago
-     */
-    public static String getRelativeTime(long time, boolean shortText) {
-        return getRelativeTime(time, shortText, true);
-    }
-
-    /**
-     * @param time           timestamp in seconds
-     * @param shortText      short or long texts
-     * @param showChronology show when it was (ago or from now)
      * @return x m[inutes [from now]]
      */
-    public static String getRelativeTime(long time, boolean shortText, boolean showChronology) {
+    public static String getRelativeTime(long time) {
         long usedTime = time * 1000L;
-        boolean future = false;
-        String chronology = "ago";
         long now = System.currentTimeMillis();
+
         if (usedTime <= 0) {
             return "???";
         }
+
         long diff;
         if (usedTime > now) {
             diff = usedTime - now;
-            chronology = "from now";
-            future = true;
         } else {
             diff = now - usedTime;
         }
-        if (!showChronology) {
-            chronology = "";
-        }
+
         if (diff < MINUTE_MILLIS) {
-            return (diff / SECOND_MILLIS) + (shortText ? "s" : " seconds " + chronology);
+            return (diff / SECOND_MILLIS) + "s";
         } else if (diff < 2 * MINUTE_MILLIS) {
-            return shortText ? "~1m" : "about a minute " + chronology;
+            return "~1m";
         } else if (diff < 50 * MINUTE_MILLIS) {
-            return diff / MINUTE_MILLIS + (shortText ? "m" : " minutes " + chronology);
+            return diff / MINUTE_MILLIS + "m";
         } else if (diff < 90 * MINUTE_MILLIS) {
-            return shortText ? "~1h" : "about an hour " + chronology;
+            return "~1h";
         } else if (diff < 24 * HOUR_MILLIS) {
-            return diff / HOUR_MILLIS + (shortText ? "h" : " hour" + (diff / HOUR_MILLIS == 1 ? "" : "s") + " " + chronology);
+            return diff / HOUR_MILLIS + "h";
         } else if (diff < 48 * HOUR_MILLIS) {
-            return shortText ? "~1d" : future ? showChronology ? "tomorrow" : "about a day" : "yesterday";
-        } else if (diff < 14 * DAY_MILLIS || !shortText) {
-            return diff / DAY_MILLIS + (shortText ? "d" : " days" + " " + chronology);
+            return "~1d";
+        } else if (diff < 14 * DAY_MILLIS) {
+            return diff / DAY_MILLIS + "d";
         }
         return ">2w";
     }
