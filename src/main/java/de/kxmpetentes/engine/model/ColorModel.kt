e69cd8b1e0952ca48c99b533f646faea272c4582
spotifynutzeer <paul.j.weber@outlook.de>
2021-03-07 16:59:17 +0100

    Updated ColorModel.kt (Automatically adds Darker and Brighter Color now)

--- src/main/java/de/kxmpetentes/engine/model/ColorModel.kt
+++ src/main/java/de/kxmpetentes/engine/model/ColorModel.kt
@@ -1,8 +1,19 @@
 package de.kxmpetentes.engine.model
 
+import sun.text.resources.ro.CollationData_ro
 import java.awt.Color
 
 data class ColorModel(private val colors: Collection<Color>) {
+
+    private val colorArrayList: ArrayList<Color> = ArrayList()
+
+    init {
+        this.colors.forEach {
+            colorArrayList.add(it.darker())
+            colorArrayList.add(it.brighter())
+            colorArrayList.add(it)
+        }
+    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -16,11 +27,13 @@ data class ColorModel(private val colors: Collection<Color>) {
     }
 
     fun getColor(): Color {
-        return colors.random()
+        return colorArrayList.random()
     }
 
     override fun hashCode(): Int {
-        return colors.hashCode()
+        return colorArrayList.hashCode()
     }
+
+
 
 }
