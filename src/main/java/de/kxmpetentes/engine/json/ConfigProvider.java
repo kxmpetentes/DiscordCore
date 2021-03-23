2d3c30efae8ee8004bdcc47eefd18387fb43814f
kxmpetentes <mikkakohnz@gmail.com>
2021-03-23 22:26:22 +0100

    Javadocs added ConfigProvider
    Signed-off-by: kxmpetentes <mikkakohnz@gmail.com>

--- src/main/java/de/kxmpetentes/engine/json/ConfigProvider.java
+++ src/main/java/de/kxmpetentes/engine/json/ConfigProvider.java
@@ -21,11 +21,16 @@ import java.io.IOException;
 @AllArgsConstructor
 public class ConfigProvider {
 
-    // TODO: 23.03.2021 javadocs
-
     private Configuration configuration;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    /**
+     * Loads the configuration from file
+     *
+     * @param file file to read
+     * @throws ConfigurationException if the configuration is null
+     * @throws IOException            if the file can't saved
+     */
     public void safeConfig(@NotNull File file) throws ConfigurationException, IOException {
         if (this.configuration == null) {
             throw new ConfigurationException("Configuration can not be null!");
@@ -34,12 +39,32 @@ public class ConfigProvider {
         objectMapper.writeValue(file, this.configuration);
     }
 
+    /**
+     * Save the configuration to file
+     *
+     * @param file          file to save the configuration to
+     * @param configuration configuration to save
+     * @throws IOException            if the file can't save
+     * @throws ConfigurationException if the configuration is null
+     */
     public void safeConfig(@NotNull File file, @NotNull Configuration configuration) throws IOException, ConfigurationException {
         setConfiguration(configuration);
         safeConfig(file);
     }
 
-    public Configuration getConfigFromFile(File file) throws IOException {
+    /**
+     * Loads the configuration from the file
+     *
+     * @param file file to load the configuration from
+     * @return the configuration that was loaded from the file
+     * @throws IOException            if the file cannot be read
+     * @throws ConfigurationException if the file is null
+     */
+    public Configuration getConfigFromFile(File file) throws IOException, ConfigurationException {
+        if (file == null) {
+            throw new ConfigurationException("File cannot be null");
+        }
+
         return objectMapper.readValue(file, Configuration.class);
     }
 }
