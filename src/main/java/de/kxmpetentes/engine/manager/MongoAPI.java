1af429cf619dd8ddc9bdc1eec80591926eef8129
kxmpetentes <mikkakohnz@gmail.com>
2021-03-24 12:14:42 +0100

    MongoAPI JavaDocs added
    Signed-off-by: kxmpetentes <mikkakohnz@gmail.com>

--- src/main/java/de/kxmpetentes/engine/manager/MongoAPI.java
+++ src/main/java/de/kxmpetentes/engine/manager/MongoAPI.java
@@ -21,14 +21,22 @@ import java.util.Collections;
 
 public class MongoAPI {
 
-    // TODO: javadocs
-
     private MongoAPI() {
     }
 
     private static MongoDatabase mongoDatabase;
     private static MongoClient client;
 
+    /**
+     * Connects to the MongoDB server
+     *
+     * @param host         hostname to connect
+     * @param port         port to connect
+     * @param username     username to connect
+     * @param authDatabase authentication database
+     * @param DB           database name
+     * @param password     password to authenticate
+     */
     public static void connect(String host, int port, String username, String authDatabase, String DB, String password) {
         MongoCredential credential = MongoCredential.createCredential(username, authDatabase, password.toCharArray());
         MongoClientSettings settings = MongoClientSettings.builder().credential(credential).applyToClusterSettings((builder)
@@ -38,23 +46,47 @@ public class MongoAPI {
         mongoDatabase = client.getDatabase(DB);
     }
 
+    /**
+     * Checks if database is connected or exists
+     *
+     * @return false if the database is not connected
+     */
     public static boolean isConnected() {
         return mongoDatabase != null;
     }
 
+    /**
+     * Get the Collection of Documents in a specific collection
+     *
+     * @param collection Collection name
+     * @return returns the MongoCollection
+     */
     public static MongoCollection<Document> getCollection(String collection) {
         return mongoDatabase.getCollection(collection);
     }
 
+    /**
+     * @return Returns the MongoClient
+     */
     public static MongoClient getClient() {
         return client;
     }
 
+    /**
+     * Get the database
+     *
+     * @return the database if it exists
+     */
     public static MongoDatabase getMongoDatabase() {
         return mongoDatabase;
     }
 
+    /**
+     * Disconnects if the database is connected
+     */
     public static void disconnect() {
-        client.close();
+        if (isConnected()) {
+            client.close();
+        }
     }
 }
