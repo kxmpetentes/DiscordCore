d37100b06abc0d62d62a7aed6b2cd31def57b351
kxmpetentes <mikkakohnz@gmail.com>
2021-03-24 12:22:44 +0100

    TopGGManager JavaDocs added
    Signed-off-by: kxmpetentes <mikkakohnz@gmail.com>

--- src/main/java/de/kxmpetentes/engine/manager/TopGGManager.java
+++ src/main/java/de/kxmpetentes/engine/manager/TopGGManager.java
@@ -1,7 +1,10 @@
 package de.kxmpetentes.engine.manager;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
 import org.discordbots.api.client.DiscordBotListAPI;
 
+import java.util.Collection;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -14,22 +17,58 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class TopGGManager {
 
-    // TODO: javadocs
-
     private final DiscordBotListAPI botListAPI;
 
+    /**
+     * Create a new TopGG API instance to manage the DiscordBotListAPI
+     *
+     * @param token API token for the botlist
+     * @param botID botid of your bot
+     */
     public TopGGManager(String token, String botID) {
         this.botListAPI = new DiscordBotListAPI.Builder().token(token).botId(botID).build();
     }
 
+    /**
+     * @return returns the BotList API instance
+     */
     public DiscordBotListAPI getBotListAPI() {
         return botListAPI;
     }
 
+    /**
+     * Set the count of your guilds
+     *
+     * @param guilds GuildServer count
+     */
     public void setServerStats(int guilds) {
         getBotListAPI().setStats(guilds);
     }
 
+    /**
+     * Sets the count of your guilds by Collection of Guilds
+     *
+     * @param guildList GuildList from the JDA
+     */
+    public void setServerStats(Collection<Guild> guildList) {
+        getBotListAPI().setStats(guildList.size());
+    }
+
+    /**
+     * Sets the count of your guilds by JDA
+     *
+     * @param jda jda instance
+     */
+    public void setServerStats(JDA jda) {
+        getBotListAPI().setStats(jda.getGuilds().size());
+    }
+
+    /**
+     * Checks the vote of a specified user id
+     *
+     * @param userID DiscordUser id
+     * @return return true if the user has voted
+     */
     public boolean hasVoted(String userID) {
         AtomicBoolean vote = new AtomicBoolean(false);
 
@@ -38,6 +77,11 @@ public class TopGGManager {
         return vote.get();
     }
 
+    /**
+     * Checks the voting multipler
+     *
+     * @return Returns true if a voting multiplier is active
+     */
     public boolean isVotingMultiplier() {
         AtomicBoolean votingMultiplier = new AtomicBoolean(false);
 
