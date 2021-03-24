51bfe41a9ba08ed8f81029f24715aec8b07f975c
kxmpetentes <mikkakohnz@gmail.com>
2021-03-24 12:48:04 +0100

    ImageUtils JavaDocs added
    Signed-off-by: kxmpetentes <mikkakohnz@gmail.com>

--- src/main/java/de/kxmpetentes/engine/utils/ImageUtils.java
+++ src/main/java/de/kxmpetentes/engine/utils/ImageUtils.java
@@ -11,16 +11,35 @@ import java.awt.*;
 
 public class ImageUtils {
 
-    //TODO Javadocs
     private ImageUtils() {
     }
 
+    /**
+     * Adds a centered text to an image
+     *
+     * @param text  Text to center
+     * @param font  Font of text
+     * @param x     x position
+     * @param y     y position
+     * @param g     graphics instance
+     * @param color color of text
+     */
     public static void addCenterText(String text, Font font, int x, int y, Graphics g, Color color) {
         g.setFont(font);
         g.setColor(color);
         g.drawString(text, Math.max(0, x - ((int) g.getFontMetrics().getStringBounds(text, g).getWidth() / 2)), y);
     }
 
+    /**
+     * Adds a right bounding text to an image
+     *
+     * @param text  text to right bounding
+     * @param font  font of text
+     * @param x     x position
+     * @param y     y position
+     * @param g     graphics instance
+     * @param color color of text
+     */
     public static void addRightText(String text, Font font, int x, int y, Graphics g, Color color) {
         g.setFont(font);
         g.setColor(color);
@@ -28,6 +47,16 @@ public class ImageUtils {
         g.drawString(text, realX, y);
     }
 
+    /**
+     * Adds a shadow text to an image
+     *
+     * @param text  text to shadow
+     * @param font  font of text
+     * @param x     x position
+     * @param y     y position
+     * @param g     graphics instance
+     * @param color color of text
+     */
     public static void addShadow(String text, Font font, int x, int y, Graphics g, Color color) {
         addText(text, font, x + 1, y + 1, g, color);
         addText(text, font, x + 1, y - 1, g, color);
@@ -35,6 +64,16 @@ public class ImageUtils {
         addText(text, font, x - 1, y - 1, g, color);
     }
 
+    /**
+     * Adds a centered shadow text to an image
+     *
+     * @param text  text to shadow
+     * @param font  font of text
+     * @param x     x position
+     * @param y     y position
+     * @param g     graphics instance
+     * @param color color of text
+     */
     public static void addCenterShadow(String text, Font font, int x, int y, Graphics g, Color color) {
         addCenterText(text, font, x + 1, y + 1, g, color);
         addCenterText(text, font, x + 1, y - 1, g, color);
@@ -42,6 +81,16 @@ public class ImageUtils {
         addCenterText(text, font, x - 1, y - 1, g, color);
     }
 
+    /**
+     * Adds a text
+     *
+     * @param text  text to add
+     * @param font  font of text
+     * @param x     x position
+     * @param y     y position
+     * @param g     graphics instance
+     * @param color color of text
+     */
     public static void addText(String text, Font font, int x, int y, Graphics g, Color color) {
         g.setFont(font);
         g.setColor(color);
