f1623ae8ce3bee7ccc08c28e2fd2f3c5d5643630
kxmpetentes <mikkakohnz@gmail.com>
2021-03-24 12:34:43 +0100

    PictureModel JavaDocs added
    Signed-off-by: kxmpetentes <mikkakohnz@gmail.com>

--- src/main/java/de/kxmpetentes/engine/model/PictureModel.java
+++ src/main/java/de/kxmpetentes/engine/model/PictureModel.java
@@ -16,8 +16,6 @@ import java.io.IOException;
 
 public class PictureModel {
 
-    // TODO: javadocs
-
     private final int widht, height;
     private final String pictureName;
     private final BufferedImage bufferedImage;
@@ -25,6 +23,14 @@ public class PictureModel {
     private Font font = Font.getFont(Font.MONOSPACED);
     private File file;
 
+    /**
+     * Create a picture
+     *
+     * @param width           width of the picture
+     * @param height          height of the picture
+     * @param pictureName     Picture name to save
+     * @param backgroundColor picture background color
+     */
     public PictureModel(int width, int height, String pictureName, Color backgroundColor) {
         this.widht = width;
         this.height = height;
@@ -35,6 +41,15 @@ public class PictureModel {
         graphics2D.setColor(backgroundColor);
     }
 
+    /**
+     * Add the Picture a text
+     *
+     * @param textColor text color
+     * @param message   text message
+     * @param x         start position to print
+     * @param y         end position to print
+     * @return return the PictureModel
+     */
     public PictureModel addText(Color textColor, String message, int x, int y) {
         graphics2D.setFont(this.font);
         graphics2D.setColor(textColor);
@@ -42,6 +57,12 @@ public class PictureModel {
         return this;
     }
 
+    /**
+     * Draw a new Rect
+     *
+     * @param color Color to draw
+     * @return return the PictureModel
+     */
     public PictureModel drawRect(Color color) {
         graphics2D.setColor(color);
         graphics2D.drawRect(0, 0, widht, height);
@@ -49,6 +70,13 @@ public class PictureModel {
         return this;
     }
 
+    /**
+     * Draw a new 3D Rect
+     *
+     * @param color  Color to draw
+     * @param raised set the 3DRect raised
+     * @return the PictureModel
+     */
     public PictureModel draw3DRect(Color color, boolean raised) {
         graphics2D.setColor(color);
         graphics2D.draw3DRect(0, 0, widht, height, raised);
@@ -56,6 +84,14 @@ public class PictureModel {
         return this;
     }
 
+    /**
+     * Draw a new circle
+     *
+     * @param color Color to draw
+     * @param x     center
+     * @param y     height
+     * @return result
+     */
     public PictureModel drawCircle(Color color, int x, int y) {
         graphics2D.setColor(color);
         graphics2D.fillOval(x, y, widht, height);
@@ -63,6 +99,12 @@ public class PictureModel {
         return this;
     }
 
+    /**
+     * Fill a Rect
+     *
+     * @param color color to draw
+     * @return result
+     */
     public PictureModel fillRect(Color color) {
         graphics2D.setColor(color);
         graphics2D.fillRect(0, 0, widht, height);
@@ -70,6 +112,12 @@ public class PictureModel {
         return this;
     }
 
+    /**
+     * Set the font
+     *
+     * @param font font to set
+     * @return result
+     */
     public PictureModel setFont(Font font) {
         this.font = font;
         this.graphics2D.setFont(font);
@@ -77,12 +125,24 @@ public class PictureModel {
         return this;
     }
 
+    /**
+     * Set the file to save
+     *
+     * @param file file to save
+     * @return the result
+     */
     public PictureModel setFile(File file) {
         this.file = file;
 
         return this;
     }
 
+    /**
+     * Save the picture as png file
+     *
+     * @return the result
+     * @throws IOException if the file cannot be saved
+     */
     public PictureModel saveToPngFile() throws IOException {
         graphics2D.dispose();
 
@@ -91,6 +151,12 @@ public class PictureModel {
         return this;
     }
 
+    /**
+     * Save the picture as jpg file
+     *
+     * @return the result
+     * @throws IOException if the file cannot be saved
+     */
     public PictureModel saveToJpgFile() throws IOException {
         graphics2D.dispose();
 
@@ -99,15 +165,23 @@ public class PictureModel {
         return this;
     }
 
-
+    /**
+     * @return the height of the picture
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * @return the width of the picture
+     */
     public int getWidht() {
         return widht;
     }
 
+    /**
+     * @return the file of the picture
+     */
     public File getFile() {
         return file;
     }
