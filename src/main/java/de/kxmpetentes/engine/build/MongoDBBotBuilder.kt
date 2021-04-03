5c7c0e32143b231acd0ff84436941f151718f3a7
SpotifyNutzer <paul.j.weber@outlook.de>
2021-04-03 18:19:56 +0200

    Added Kotlin docs to MongoDBBotBuilder.kt

--- src/main/java/de/kxmpetentes/engine/build/MongoDBBotBuilder.kt
+++ src/main/java/de/kxmpetentes/engine/build/MongoDBBotBuilder.kt
@@ -20,6 +20,12 @@ class MongoDBBotBuilder {
     private val eventListeners: Collection<EventListener>
     private val commandExecutors: Collection<Command>
 
+    /**
+     * @param discordCore       the discordcore the bot should run with
+     * @param token             the token that should be used to build the bot
+     * @param onlineStatus      the onlineStatus of the bot
+     * @param activity          the activity of the bot
+     */
     constructor(
         discordCore: DiscordCore,
         token: String,
@@ -38,6 +44,15 @@ class MongoDBBotBuilder {
         discordCore.enableMongoDB()
     }
 
+    /**
+     * @param discordCore       the discordcore the bot should run with
+     * @param token             the token that should be used to build the bot
+     * @param onlineStatus      the onlineStatus of the bot
+     * @param activity          the activity of the bot
+     * @param gatewayInents     the gatewayintents of the bot
+     * @param eventListeners    all eventlisteners of the bot
+     * @param commandExecutors  all commandexecutors of the bot
+     */
     constructor(
         discordCore: DiscordCore,
         token: String,
@@ -56,18 +71,30 @@ class MongoDBBotBuilder {
         this.commandExecutors = commandExecutors
     }
 
+    /**
+     * @return the token of the bot
+     */
     fun getToken(): String {
         return token
     }
 
+    /**
+     * @return the activity of the bot
+     */
     fun getActivity(): Activity {
         return activity
     }
 
+    /**
+     * @return the online status of the bot
+     */
     fun getOnlineStatus(): OnlineStatus {
         return onlineStatus
     }
 
+    /**
+     * @return the shardmanager of the bot
+     */
     @Throws(LoginException::class)
     fun getDefaultShardManager(): ShardManager {
         if (gatewayInents.isNotEmpty() && eventListeners.isNotEmpty() && commandExecutors.isNotEmpty()) {
@@ -82,6 +109,9 @@ class MongoDBBotBuilder {
         return BotCreateManager(token, activity, onlineStatus).defaultShardManager
     }
 
+    /**
+     * @return the commandshardmanager of the bot
+     */
     fun getCommandShardManager(): ShardManager {
         if (gatewayInents.isNotEmpty() && eventListeners.isNotEmpty() && commandExecutors.isNotEmpty()) {
             return BotCreateManager(discordCore,
@@ -95,6 +125,9 @@ class MongoDBBotBuilder {
         return BotCreateManager(token, activity, onlineStatus).commandShardManager
     }
 
+    /**
+     * @return the discordcore the bot is running with
+     */
     fun getDiscordCore(): DiscordCore {
         return discordCore
     }
